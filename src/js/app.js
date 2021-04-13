3d5f5247e8792ba5ef0494d42a9fb02b35f842a9
Igor Nikolauk <nib287@yandex.ru>
2021-04-13 21:08:44 +0300

    fix: edit url

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import '../css/style.css';
 import Controller from './controller.js';
 
-const controller = new Controller('ws://ahj-messenger.herokuapp.com./ws');
+const controller = new Controller('ws://ahj-messenger.herokuapp.com/ws');
 controller.init();
